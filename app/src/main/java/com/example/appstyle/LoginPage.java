d82145142e6e97d48b9b648533a7b63685a736d9
pedroberri <pedroberri08@gmail.com>
2023-06-14 21:42:04 -0300

    LoginPageDone

---
+++ app/src/main/java/com/example/appstyle/LoginPage.java
@@ -0,0 +1,16 @@
+package com.example.appstyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        getSupportActionBar().hide();
+    }
+}
