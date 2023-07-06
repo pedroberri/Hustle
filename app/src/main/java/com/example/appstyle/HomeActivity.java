c3f3d19838b3474a017cf65421f5d6fa2f7202b8
pedroberri <pedroberri08@gmail.com>
2023-07-05 21:14:54 -0300

    HomePage 50%

--- app/src/main/java/com/example/appstyle/HomeActivity.java
+++ app/src/main/java/com/example/appstyle/HomeActivity.java
@@ -10,7 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
     }
-
-
 }
