893342194daad1d43a010ad1678af9c03391da3c
pedroberri <pedroberri08@gmail.com>
2023-07-12 18:55:41 -0300

    Merge dos projetos

--- app/src/main/java/com/example/appstyle/LoginPage.java
+++ app/src/main/java/com/example/appstyle/LoginPage.java
@@ -1,29 +1,95 @@
 package com.example.appstyle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class LoginPage extends AppCompatActivity {
+
+    private EditText edit_email, edit_password;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        getSupportActionBar().hide();
+        InicializarCampos();
 
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String password = edit_password.getText().toString();
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            TelaPrincipal();
+                        } else {
+                            Snackbar snackbar = Snackbar.make(v, "Invalid email or password", Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.RED);
+                            snackbar.setTextColor(Color.WHITE);
+                            snackbar.show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+        //Deixar sublinhado
         TextView cadastroText = findViewById(R.id.cadastroText);
         SpannableString spannableString = new SpannableString(cadastroText.getText());
         spannableString.setSpan(new UnderlineSpan(), 0, spannableString.length(), 0);
         cadastroText.setText(spannableString);
+
+
+        //Ir para página de cadastro
         cadastroText.setOnClickListener(view -> {
             Intent intent = new Intent(LoginPage.this, RegisterPage.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            TelaPrincipal();
+        }
+    }
+
+    public void TelaPrincipal() {
+        Intent intent = new Intent(LoginPage.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void InicializarCampos() {
+        edit_email = findViewById(R.id.nomeUser);
+        edit_password = findViewById(R.id.senha);
+        login_button = findViewById(R.id.buttonLogar);
     }
 }
--- app/src/main/java/com/example/appstyle/RegisterPage.java
+++ app/src/main/java/com/example/appstyle/RegisterPage.java
@@ -1,37 +1,120 @@
 package com.example.appstyle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class RegisterPage extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
+import java.util.Objects;
+
+public class RegisterPage extends AppCompatActivity {
+    private EditText edit_email, edit_password, edit_verify_password;
+    private Button submit_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        getSupportActionBar().hide();
+
+        InicializarCampos();
+
+        //Sublima texto
         TextView loginText = findViewById(R.id.loginText);
         SpannableString spannableString = new SpannableString(loginText.getText());
         spannableString.setSpan(new UnderlineSpan(), 0, spannableString.length(), 0);
         loginText.setText(spannableString);
 
-//        ExerciseApiService exerciseApiService = new ExerciseApiService();
-//        exerciseApiService.getExercises(value -> {
-//            System.out.println(value);
-//        });
+        //Click em registrar-se
+        submit_button.setOnClickListener(v -> {
+            String email = edit_email.getText().toString();
+            String senha = edit_password.getText().toString();
+            String confimaSenha = edit_verify_password.getText().toString();
+
+            String mensagemErro = "All fields are required";
+            Snackbar snackbar = Snackbar.make(v, mensagemErro, Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.RED);
+            snackbar.setTextColor(Color.WHITE);
+
+            if(email.isEmpty() || senha.isEmpty() || confimaSenha.isEmpty()) {
+                snackbar.show();
+            } else if (!senha.equals(confimaSenha)) {
+                snackbar.setText("The passwords are different");
+                snackbar.show();
+            } else {
+                CadastrarUsuario(email, senha, v);
+            }
 
 
+        });
+
+        // Vai para página de login
         loginText.setOnClickListener(view -> {
             Intent intent = new Intent(RegisterPage.this, LoginPage.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         });
+
+    }
+
+    private void CadastrarUsuario(String email, String password, View v) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    TelaPrincipal();
+                }else {
+                    String erro = "";
+                    try {
+                        throw Objects.requireNonNull(task.getException());
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "The password must contain at least 6 characters";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        erro = "A use with this email address has already been registeredA use with this email address has already been registered";
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "This email is not valid";
+                    }
+                    catch (Exception e) {
+                        erro = "Something wrong, try to create an account later";
+                    } finally {
+                        Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.RED);
+                        snackbar.setTextColor(Color.WHITE);
+                        snackbar.show();
+                    }
+                }
+            }
+        });
+    }
+
+    public void TelaPrincipal() {
+        Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void InicializarCampos() {
+        edit_email = findViewById(R.id.email);
+        edit_password = findViewById(R.id.senha);
+        edit_verify_password = findViewById(R.id.confirmarSenha);
+        submit_button = findViewById(R.id.submit_button);
     }
 }
