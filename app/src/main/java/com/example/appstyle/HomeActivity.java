1bf14d2bb62932ed95cfb5d484d936773ac35786
pedroberri <pedroberri08@gmail.com>
2023-07-19 20:23:26 -0300

    travou

--- app/src/main/java/com/example/appstyle/HomeActivity.java
+++ app/src/main/java/com/example/appstyle/HomeActivity.java
@@ -1,16 +1,46 @@
 package com.example.appstyle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.appstyle.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        binding.navBar.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    break;
+                case R.id.search:
+                    break;
+                case R.id.workout:
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home)
     }
 }
