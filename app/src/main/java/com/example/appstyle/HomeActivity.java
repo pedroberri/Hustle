3f7a5e380bfd5c5a81c6ad352b9baf90ed1c2560
kaueTeixxeira <kaueteixerak9@hotmail.com>
2023-07-19 20:57:39 -0300

    Logout working

--- app/src/main/java/com/example/appstyle/HomeActivity.java
+++ app/src/main/java/com/example/appstyle/HomeActivity.java
@@ -2,9 +2,17 @@ package com.example.appstyle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView logout_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,26 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         getSupportActionBar().hide();
+
+        InicializarCampos();
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+    }
+
+    private void InicializarCampos() {
+        logout_button = findViewById(R.id.logout);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(HomeActivity.this, LoginPage.class);
+        startActivity(intent);
+        finish();
     }
 }
