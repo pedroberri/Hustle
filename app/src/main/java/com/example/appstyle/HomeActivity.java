276e6701f6d4f1c1a93ac36fe997bfee3b98afc7
pedroberri <pedroberri08@gmail.com>
2023-07-19 21:00:39 -0300

    fragmentos criados

--- app/src/main/java/com/example/appstyle/HomeActivity.java
+++ app/src/main/java/com/example/appstyle/HomeActivity.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.example.appstyle.databinding.ActivityHomeBinding;
+import com.example.appstyle.fragment.HomeFragment;
+import com.example.appstyle.fragment.SearchFragment;
+import com.example.appstyle.fragment.WorkoutFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,28 +22,28 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
+        replaceFragment(new HomeFragment());
 
         binding.navBar.setOnItemSelectedListener(item -> {
-
             switch (item.getItemId()){
-
                 case R.id.home:
+                    replaceFragment(new HomeFragment());
                     break;
                 case R.id.search:
+                    replaceFragment(new SearchFragment());
                     break;
                 case R.id.workout:
+                    replaceFragment(new WorkoutFragment());
                     break;
-
             }
-
             return true;
         });
     }
 
     private void replaceFragment(Fragment fragment) {
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.home)
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
