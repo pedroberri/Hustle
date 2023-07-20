327060483d63eab1f5114221dede29b9fd15d815
kaueTeixxeira <kaueteixerak9@hotmail.com>
2023-07-19 21:50:38 -0300

    Little fixes

--- app/src/main/java/com/example/appstyle/HomeActivity.java
+++ app/src/main/java/com/example/appstyle/HomeActivity.java
@@ -8,12 +8,11 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.appstyle.databinding.ActivityHomeBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
-import com.example.appstyle.databinding.ActivityHomeBinding;
 import com.example.appstyle.fragment.HomeFragment;
 import com.example.appstyle.fragment.SearchFragment;
 import com.example.appstyle.fragment.WorkoutFragment;
@@ -32,17 +31,17 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
         replaceFragment(new HomeFragment());
-        InicializarCampos();
+//        InicializarCampos();
 
-        logout_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout();
-            }
-        });
-      
-      binding.navBar.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
+//        logout_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                logout();
+//            }
+//        });
+
+        binding.navBar.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
                 case R.id.home:
                     replaceFragment(new HomeFragment());
                     break;
@@ -57,24 +56,22 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    private void InicializarCampos() {
-        logout_button = findViewById(R.id.logout);
-    }
+//    private void InicializarCampos() {
+//        logout_button = findViewById(R.id.logout);
+//    }
 
-    private void logout() {
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(HomeActivity.this, LoginPage.class);
-        startActivity(intent);
-        finish();
-    }
-        
+//    private void logout() {
+//        FirebaseAuth.getInstance().signOut();
+//        Intent intent = new Intent(HomeActivity.this, LoginPage.class);
+//        startActivity(intent);
+//        finish();
+//    }
 
-        
 
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
 }
